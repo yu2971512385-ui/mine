045b16f68764f52e720ee9baf233cef68e0a5c29
Andy <yu2971512385-ui@users.noreply.github.com>
2026-08-17 17:59:29 +0800

    Refresh cached site assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'andy-growth-v3'
+const CACHE_NAME = 'andy-growth-v4'
 const APP_SHELL = [
   './',
   './index.html',
@@ -51,12 +51,14 @@ self.addEventListener('fetch', (event) => {
   }
 
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-      if (response.ok) {
-        const copy = response.clone()
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
-      }
-      return response
-    })),
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+        }
+        return response
+      })
+      .catch(() => caches.match(event.request, { ignoreSearch: true })),
   )
 })
